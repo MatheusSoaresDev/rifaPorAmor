e3e7bd490d4453fb085f02719081a1bd5f7d9f59
Matheus Soares <matheus_onlive@live.com>
2021-10-06 22:43:59 -0300

    Estrutura inicial do projeto criada. Esboço da pagina de login criada.

---
+++ assets/classes/Usuario.php
@@ -0,0 +1,8 @@
+<?php
+
+class Usuario
+{
+    private string $nome;
+    private string $email;
+    private string $celular;
+}
---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <link href="assets/css/login.css" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body>
+
+<div class="box">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <label for="exampleFormControlInput1" class="form-label">Digite seu email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+    </div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <label for="exampleFormControlInput1" class="form-label">Digite sua senha</label>
+        <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="">
+    </div>
+
+    <a href="#"><small> Esqueci minha senha </small></a>
+
+    <div class="buttons">
+        <button type="button" class="btn btn-primary entrar-button">ENTRAR</button>
+    </div>
+
+</div>
+
+
+<script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+</body>
+</html>
