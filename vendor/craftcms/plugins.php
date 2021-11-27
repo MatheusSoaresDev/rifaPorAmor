c155e4b93394cbe2b6553eb1e31e7aae65452843
Matheus Soares <matheus_onlive@live.com>
2021-11-27 11:58:01 -0300

    vendor

---
+++ vendor/craftcms/plugins.php
@@ -0,0 +1,7 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+$rootDir = dirname(dirname(__DIR__));
+
+return array (
+);
---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,24 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yii2tech/ar-softdelete' => 
+  array (
+    'name' => 'yii2tech/ar-softdelete',
+    'version' => '1.0.4.0',
+    'alias' => 
+    array (
+      '@yii2tech/ar/softdelete' => $vendorDir . '/yii2tech/ar-softdelete/src',
+    ),
+  ),
+  'creocoder/yii2-nested-sets' => 
+  array (
+    'name' => 'creocoder/yii2-nested-sets',
+    'version' => '0.9.0.0',
+    'alias' => 
+    array (
+      '@creocoder/nestedsets' => $vendorDir . '/creocoder/yii2-nested-sets/src',
+    ),
+  ),
+);
